72a8d42bd49430c63e3aaad4ba618dfa115cf3d0
reciokaue <kaue.recio2@gmail.com>
2024-01-07 21:33:13 -0300

    home hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import '../styles/globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +22,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          roboto.className,
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
