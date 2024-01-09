7f9adeb413f03e886df173c932c26ce0b8d798ec
reciokaue <kaue.recio2@gmail.com>
2024-01-09 10:42:45 -0300

    setting cookie

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,8 @@
+import { setCookie } from 'nookies'
+
 import { prisma } from '@/lib/prisma'
 
-export async function POST(req: Request) {
+export async function POST(req: Request, res: Response) {
   const data = await req.json()
   const { name, username } = data
 
@@ -19,6 +21,11 @@ export async function POST(req: Request) {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@dev.call:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return Response.json(user, { status: 201 })
